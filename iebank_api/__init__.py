bf95e97e7fcda0b22776d680634e482966bedca8
Daniel Mora <danielmora@Daniels-MacBook-Air-2.local>
2024-10-14 20:24:21 +0200

    Please lord, I your humble servant ask you to make this run

--- iebank_api/__init__.py
+++ iebank_api/__init__.py
@@ -2,8 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
+from sqlalchemy import text
+
+
+import dotenv
+
+dotenv.load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///local.db'
+
+db = SQLAlchemy(app)
 
 # Select environment based on the ENV environment variable
 if os.getenv('ENV') == 'local':
@@ -16,11 +26,15 @@ elif os.getenv('ENV') == 'ghci':
     print("Running in github mode")
     app.config.from_object('config.GithubCIConfig')
 
-db = SQLAlchemy(app)
 
 from iebank_api.models import Account
 
 with app.app_context():
+    # # Uncomment to add the country column to the account table
+    # query = text("ALTER TABLE account ADD COLUMN country VARCHAR(32)")
+    # db.session.execute(query)
+    # db.session.commit()
+
     db.create_all()
 CORS(app)
 
